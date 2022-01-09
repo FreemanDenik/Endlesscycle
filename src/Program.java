229eb1488d9c4e1c19fcee65dac77f8e38d3630a
ImFreeman <denikvy@gmail.com>
2022-01-09 12:23:37 +0300

    Initial commit

---
+++ src/Program.java
@@ -0,0 +1,9 @@
+public class Program {
+    public static void main(String[] str){
+        int start = Integer.MAX_VALUE - 1;
+        for(int i = 0; i < start + 1; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
